ed27efc8ee39c68896a0cdddfc67ee33da27dc02
azeno776 <enzo776b@outlook.fr>
2026-09-23 17:16:33 +0200

    src

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+class Server {
+private:
+    //Atributs
+public:
+    //methodes
+};
+
+class Sensor {
+private:
+    //Atributs
+public:
+    //methodes
+};
+
+class Scheduler {
+private:
+    //Atributs
+public:
+    //methodes
+};
+
